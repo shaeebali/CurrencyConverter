55ce3d1761fd89d201f03edb92f481077f4b1863
shaeeb <shaeebali@gmail.com>
2022-12-10 12:32:06 +0000

    consume first API and add key to .env

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//import/require dependencies
+require('dotenv').config();
+
+// API Providers
+// IP Data
+const ipdata = {
+  baseurl: 'https://api.ipdata.co',
+  key: process.env.API_KEY_IPDATA,
+  
+  //create a method
+  currency: function () {
+    return `${this.baseurl}/currency?api-key=${ipdata.key}`
+  }
+}
 
 
